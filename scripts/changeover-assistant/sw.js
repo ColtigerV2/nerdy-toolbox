55d07233779005db0fecb99fc4808033c5f91d1d
ColtigerV2 <martin.robin2811@gmail.com>
2026-05-03 01:49:46 +0200

    Bump cache after removing top pump fields

--- scripts/changeover-assistant/sw.js
+++ scripts/changeover-assistant/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'changeover-assistant-v10';
+const CACHE_NAME = 'changeover-assistant-v11';
 const ASSETS = [
   './',
   './index.html',
-  './style.css?v=5',
-  './script.js?v=5',
+  './style.css?v=6',
+  './script.js?v=6',
   './manifest.json',
   './icons/icon.svg'
 ];
