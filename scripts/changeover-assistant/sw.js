ace529e98e75616c7e8a417a343f533a1af716cc
ColtigerV2 <martin.robin2811@gmail.com>
2026-05-03 01:31:45 +0200

    Bump cache for automatic OCR flow

--- scripts/changeover-assistant/sw.js
+++ scripts/changeover-assistant/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'changeover-assistant-v8';
+const CACHE_NAME = 'changeover-assistant-v9';
 const ASSETS = [
   './',
   './index.html',
-  './style.css?v=3',
-  './script.js?v=3',
+  './style.css?v=4',
+  './script.js?v=4',
   './manifest.json',
   './icons/icon.svg'
 ];
