3f4fbd363c11bef6f6b787c84ad82f3cde57dabb
ColtigerV2 <martin.robin2811@gmail.com>
2026-05-03 04:37:28 +0200

    Bump cache for roll speed correction

--- scripts/roll-change-calculator/sw.js
+++ scripts/roll-change-calculator/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'roll-change-calculator-v11';
+const CACHE_NAME = 'roll-change-calculator-v12';
 const ASSETS = [
   './',
   './index.html',
-  './style.css?v=11',
-  './script.js?v=11',
+  './style.css?v=12',
+  './script.js?v=12',
   './manifest.json',
   './icons/icon.svg'
 ];
